557b1962f2d5b7fc34eaba28f375f76e6b7d64b0
m1duza <ivan.zubcov228@gmail.com>
2024-11-14 23:11:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  base: '/pat-proect2/', // Замените 'your-repo-name' на имя вашего репозитория
+  base: '/pat-proect/', // Замените 'your-repo-name' на имя вашего репозитория
 
 });
